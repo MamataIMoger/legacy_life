d40cef372f66f9d8a425cf00d314437e5d155dc5
Madhunika K J <madhunikakj326@gmail.com>
2025-08-21 00:51:42 +0530

    Modified story admin to add stories manually

--- stories/admin.py
+++ stories/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Story
 
-# Register your models here.
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author_name', 'is_featured', 'status', 'created_at')
+    list_filter = ('is_featured', 'status', 'created_at')
+    search_fields = ('title', 'author_name', 'summary', 'body')
+
+    # ✅ mark system fields as read-only
+    readonly_fields = ('slug', 'created_at', 'updated_at')
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'author_name', 'summary', 'body', 'cover', 'category')
+        }),
+        ('Publishing', {
+            'fields': ('status', 'is_featured', 'published_at', 'word_file')
+        }),
+        ('System Info', {
+            'fields': ('slug', 'views', 'created_at', 'updated_at'),
+        }),
+    )
